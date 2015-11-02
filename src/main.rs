2202182844a4a0fa2cd59dd9cfb33f3d3b292144
Christopher Jordan <cgjordan@email.arizona.edu>
2015-11-01 18:04:30 -0700

    Moved to GitHub!

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate rand;
+
+use rand::Rng;
+
+fn main() {
+    let mut stay = 0;
+    let mut switch = 0;
+    let count = 1000000;
+    let talkative = false; // Lots of output slows down the program
+    
+    for game in 0..count {
+        if talkative { println!("Game number {}", game+1); }
+    
+        let car = rand::thread_rng().gen_range(1, 4); //upper bound is exclusive
+        if talkative { println!("  The car is behind door #{}.", car); }
+        
+        let pick = rand::thread_rng().gen_range(1, 4);
+        if talkative { println!("  The player picks door #{}.", pick); }
+        
+        let mut open;
+        loop {
+            open = rand::thread_rng().gen_range(1, 4);
+            if !((open == car) || (open == pick)) { break; }
+        }
+        if talkative { println!("  The host opens door #{}, revealing a goat behind it.", open); }
+        
+        let mut offer;
+        loop {
+            offer = rand::thread_rng().gen_range(1, 4);
+            if !((offer == open) || (offer == pick)) { break; }
+        }
+        if talkative { println!("  The host offers to let the player switch to door #{}.", offer); }
+        
+        if pick == car {
+            stay += 1;
+            if talkative { println!("  The door the player picked was the car!"); }
+        } else if offer == car {
+            switch += 1;
+            if talkative { println!("  The door the host offered was the car!"); }
+        } else {
+            // The game is a scam and there is no car.
+            panic!();
+        }
+    }
+    
+    println!("Finished simulating {} games.", count);
+    println!("Number of cars won by staying:   {}", stay);
+    println!("Number of cars won by switching: {}", switch);
+}
